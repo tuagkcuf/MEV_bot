0d967c3924baabc7884838bdbbfac7925efd0c97
tuagkcuf <jokersms222@gmail.com>
2023-05-28 09:43:59 +0300

    add connection between provider and flashbots connection'

--- bot.js
+++ bot.js
@@ -8,6 +8,19 @@ import * as dotenv from "dotenv";
 
 dotenv.config();
 
+// setup the provider
 const provider = new providers.JsonRpcProvider(PROVIDER_LINK);
 
+// create a unique flashbots id
 const authSigner = new Wallet(PRIVATE_KEY, provider);
+
+// create the flashbots provider insider a start function
+const start = async () => {
+    const flashbotsProvider = await FlashbotsBundleProvider.create(
+        provider,
+        authSigner,
+        FLASHBOTS_PROVIDER_LINK
+    );
+};
+
+start();
